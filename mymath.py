052dcaaae5bcbf127b9b060d820631abe93b0628
box-in <18380877+box-in@users.noreply.github.com>
2018-09-17 17:07:45 +0900

    dp as knapsack

--- mymath.py
+++ mymath.py
@@ -91,8 +91,52 @@ def decrypt_rsa(crypto, d, n=971399613123842390750807211311882448420515153055720
     return dec
 
 
+def dp(ary, limit, _index=0, _values=None, _stats=None):
+    """
+    Dynamic Programming as Knapsack problem
+    :param ary: e.g. [[cost, value], [cost, value], [cost, value]...]
+    :param limit: cost limit
+    :param _index:
+    :param _values: memorized table
+    :param _stats: select item flag
+    :return:
+    """
+    if _values is None:  # init memo table
+        _stats = [False] * len(ary)
+        _values = [[-1 for _ in range(len(ary))] for _ in range(limit + 1)]
+    if _index == len(ary):  # last node
+        return 0, _stats
+    if _values[limit][_index] > -1:  # already discovered
+        return _values[limit][_index], _stats
+    if limit < ary[_index][0]:  # limit overflow
+        return dp(ary, limit, _index + 1, _values, _stats)
+    a = dp(ary, limit - ary[_index][0], _index + 1, _values, _stats)[0] + ary[_index][1]
+    b = dp(ary, limit, _index + 1, _values, _stats)[0]
+    if a > b and not _stats[_index]:
+        _stats[_index] = True
+    _values[limit][_index] = max(a, b)
+    return _values[limit][_index], _stats
+
+
 def main():
+    Q = [
+        [440, 84], [61, 55], [149, 37], [959, 28], [363, 8],
+        [30, 13], [758, 18], [794, 6], [110, 86], [498, 61],
+        [327, 80], [162, 42], [232, 71], [573, 50], [115, 92],
+        [239, 74], [238, 59], [236, 97], [626, 76], [656, 39],
+        [318, 78], [772, 57], [877, 7], [141, 83], [52, 75],
+        [239, 74], [238, 59], [236, 97], [626, 76], [656, 39],
+        [318, 78], [772, 57], [877, 7], [141, 83], [52, 75],
+        [239, 74], [238, 59], [236, 97], [626, 76], [656, 39],
+        [318, 78], [772, 57], [877, 7], [141, 83], [52, 75],
+        [468, 93], [674, 54], [678, 34], [332, 14], [602, 61]
+    ]
+    size = 10000
+    maxvalue, flags = dp(Q, size)
+    selected = [Q[i] for i in range(len(flags)) if flags[i]]
+    print("max=%d select=%s" % (maxvalue, selected))
     pass
+
 
 if __name__  == "__main__":
     main()
